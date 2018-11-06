6d1c364d64782828d20e2d6343822c621214be1e
Zander Nickle <znickle24@icloud.com>
2018-11-06 14:11:43 -0700

    Initial creating of SpringBoot app along with adding the .gitignore.

---
+++ src/main/java/com/garbageview/garbageview/GarbageviewApplication.java
@@ -0,0 +1,14 @@
+package com.garbageview.garbageview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageviewApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(GarbageviewApplication.class, args);
+  }
+}
