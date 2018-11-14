9a130c51c700d12a0870ded77926c122a71cff41
Zander Nickle <znickle24@icloud.com>
2018-11-14 16:14:32 -0700

    Verified that the repository is storing garbage collection events in the repo. Will then work on getting GC to a JSON format to send through socket to react app.

--- src/main/java/com/garbageview/garbageview/GarbageViewController.java
+++ src/main/java/com/garbageview/garbageview/GarbageViewController.java
@@ -1,12 +1,12 @@
 package com.garbageview.garbageview;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class GarbageViewController
-{
-  //TODO: update mapping to display the correct information from the react app
-  @RequestMapping("/")
-  public String input() { return "You are in the garbage view application"; }
-}
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class GarbageViewController
+//{
+//  //TODO: update mapping to display the correct information from the react app
+//  @RequestMapping("/")
+//  public String input() { return "You are in the garbage view application"; }
+//}
--- src/main/java/com/garbageview/garbageview/GarbageviewApplication.java
+++ src/main/java/com/garbageview/garbageview/GarbageviewApplication.java
@@ -3,14 +3,13 @@ package com.garbageview.garbageview;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Collection;
 
 @SpringBootApplication
 public class GarbageviewApplication
@@ -23,9 +22,21 @@ public class GarbageviewApplication
   }
 
   @Bean
-  CommandLineRunner runner () {
+  CommandLineRunner runner (GarbageCollectionsRepo gcr) {
    return args -> {
-     GCInformation.installGCMonitoring();
+     //as soon as the beans are ready, install the monitor for each gc event
+//     GCInformation.installGCMonitoring();
+
+     //for initial testing before running with the actual gcMonitor, testing to make sure that these are saving in the repo
+     gcr.save( new GarbageCollectionsEntity("PS1", "old", 21, "GCInfoName", "End of Loop",
+         1000, "UsedMemoryAfter", "UsedMemoryBefore", 25));
+     gcr.save( new GarbageCollectionsEntity("PS1", "new", 21, "GCInfoName", "End of Loop",
+         1000, "UsedMemoryAfter", "UsedMemoryBefore", 26));
+     gcr.save( new GarbageCollectionsEntity("Parallel", "new", 21, "GCInfoName", "End of Loop",
+         1000, "UsedMemoryAfter", "UsedMemoryBefore", 27));
+     //print out each of these to make sure they're saved
+     gcr.findAll().forEach(System.out::println);
+
    };
   }
 }
@@ -37,3 +48,61 @@ public class GarbageviewApplication
 //
 //}
 
+
+
+
+
+
+
+interface GarbageCollectionsRepo extends JpaRepository<GarbageCollectionsEntity, Long>
+{
+  //allows you to grab all collections based on a particular type
+  Collection<GarbageCollectionsEntity> findByGcType (String ct);
+}
+
+
+@Entity
+class GarbageCollectionsEntity
+{
+  @Id
+  @GeneratedValue
+  private long id;
+
+  private String garbagecollector;
+  private String gcType;
+  private long gcInfoId;
+  private String gcInfoName;
+  private String gcInfoCause;
+  private long gcInfoDuration;
+  private String memoryUsageAfterGC;
+  private String memoryUsageBeforeGC;
+  private long gcOverhead;
+
+  public GarbageCollectionsEntity(String garbagecollector, String gcType, long gcInfoId, String gcInfoName, String gcInfoCause,
+                                  long gcInfoDuration, String memoryUsageAfterGC, String memoryUsageBeforeGC, long gcOverhead) {
+    this.garbagecollector = garbagecollector;
+    this.gcType = gcType;
+    this.gcInfoId = gcInfoId;
+    this.gcInfoName = gcInfoName;
+    this.gcInfoCause = gcInfoCause;
+    this.gcInfoDuration = gcInfoDuration;
+    this.memoryUsageAfterGC = memoryUsageAfterGC;
+    this.memoryUsageBeforeGC = memoryUsageBeforeGC;
+    this.gcOverhead = gcOverhead;
+  }
+
+  GarbageCollectionsEntity() { //JPA requires this separate constructor
+
+  }
+
+  @Override
+  public String toString() {
+    return "GarbageCollections {" + "id=" + id + ", garbagecollector=" + garbagecollector + ", gcType" + gcType +
+        ", gcInfoId=" + gcInfoId + ", gcInfoName=" + gcInfoName + ", gcInfoCause=" + gcInfoCause + ", gcInfoDuration=" +
+        gcInfoDuration + ", memoryUsageAfterGC=" + memoryUsageAfterGC + ", memoryUsageBeforeGC=" + memoryUsageBeforeGC +
+        ", gcOverhead=" + gcOverhead + '\'' + "}";
+  }
+
+  public long getId () { return id; }
+
+}
