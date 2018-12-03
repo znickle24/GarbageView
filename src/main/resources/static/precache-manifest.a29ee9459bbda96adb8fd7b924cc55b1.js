d811ea087fd58c679c79d586c2ea8ac7621c508d
Mason West <mew0830@gmail.com>
2018-12-03 13:49:56 -0700

    got the basic react template working. Issues with bundling it all together to get the build stuff to work right.

---
+++ src/main/resources/static/precache-manifest.a29ee9459bbda96adb8fd7b924cc55b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "371938f62a8b5d910609",
+    "url": "/static/js/main.371938f6.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "371938f62a8b5d910609",
+    "url": "/static/css/main.b6c46313.chunk.css"
+  },
+  {
+    "revision": "d05d053f7ad18b2d6c57ef26a4753fb6",
+    "url": "/index.html"
+  }
+];
